019d9d234efea6bce912b3ac2bd2b7ce001aadd4
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-01-31 13:43:44 +0900

    work1

--- Capture/capturescreen.py
+++ Capture/capturescreen.py
@@ -10,11 +10,80 @@ def finding_character(src):
     result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
     h, w = temp.shape
+
     return (min_loc), h, w
 
-def finding_box(src):
+def finding_boxes(src):
     src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
     temp = cv.imread("./data/box.png")
+    temp = cv.cvtColor(temp, cv.COLOR_RGB2GRAY)
+    points = []
+    h, w = temp.shape
+    while True:
+         result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
+         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+         if min_val<200000:
+             sx, sy = min_loc
+             for x in range(sx, sx+w):
+                 for y in range(sy , sy + h):
+                     try:
+                         src[y][x] = np.float32(-10000)  # -MAX
+                     except IndexError:  # ignore out of bounds
+                         pass
+             point = (min_loc[0],min_loc[1],h,w)
+             points.append(point)
+         else:
+             break
+    return points
+
+
+def finding_signs(src):#capturing the treasure box's sign
+    src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+    temp = cv.imread("./data/sign.png")
+    temp = cv.cvtColor(temp, cv.COLOR_RGB2GRAY)
+    points = []
+    h, w = temp.shape
+    while True:
+        result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+        if min_val < 200000:
+            sx, sy = min_loc
+            for x in range(sx, sx + w):
+                for y in range(sy, sy + h):
+                    try:
+                        src[y][x] = np.float32(-10000)  # -MAX
+                    except IndexError:  # ignore out of bounds
+                        pass
+            point = (min_loc[0], min_loc[1], h, w)
+            points.append(point)
+        else:
+            break
+    return points
+
+def finding_gems(src):
+    src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+    temp = cv.imread("./data/hero.png")
+    temp = cv.cvtColor(temp, cv.COLOR_RGB2GRAY)
+    result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    h, w = temp.shape
+    return (min_loc), h, w
+
+def finding_entities(src):
+    src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+
+    temp = cv.imread("./data/entity/entity.png")
+    temp = cv.cvtColor(temp, cv.COLOR_RGB2GRAY)
+    result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    h, w = temp.shape
+    return (min_loc), h, w
+
+def finding_boss_entities(src):
+    src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
+    temp = cv.imread("./data/hero.png")
     temp = cv.cvtColor(temp, cv.COLOR_RGB2GRAY)
     result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
@@ -25,9 +94,10 @@ def finding_box(src):
 def main():
     img = cv.imread("test1.png")
 
-    pt,h,w = finding_character(img)
+    result = finding_signs(img)
 
-    cv.rectangle(img, (pt[0], pt[1]), (pt[0] + w, pt[1] + h), (0, 0, 200), 3)
+    for (x,y,h,w)  in result:
+        cv.rectangle(img, (x, y), (x + w, y+ h), (0, 0, 200), 3)
     cv.imwrite("output.png", img)
 
 
