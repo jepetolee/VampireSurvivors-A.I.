0fbf1251a1fc5f6596d02482e6b5cfec0dada348
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-02-02 06:57:36 +0900

    new

--- Capture/capturescreen.py
+++ Capture/capturescreen.py
@@ -8,9 +8,18 @@ def finding_character(src):
     temp = cv.cvtColor(temp, cv.COLOR_RGB2GRAY)
     result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-    h, w = temp.shape
+    temp2 = cv.imread("./data/hero2.png")
+    temp2 = cv.cvtColor(temp2, cv.COLOR_RGB2GRAY)
+    result = cv.matchTemplate(src, temp2, cv.TM_SQDIFF)
+    min_val2, max_val2, min_loc2, max_loc2 = cv.minMaxLoc(result)
+    if min_val < min_val2:
+        min_loc_def = min_loc
+        h, w = temp.shape
+    else:
+        min_loc_def = min_loc2
+        h, w = temp2.shape
 
-    return min_loc, h, w
+    return min_loc_def[0], min_loc_def[1], h, w
 
 
 def finding_boxes(src):
@@ -22,12 +31,12 @@ def finding_boxes(src):
     result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
     while True:
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-        if min_val < 200000:
+        if min_val < 1760000:
             sx, sy = min_loc
             for x in range(sx, sx + w):
                 for y in range(sy, sy + h):
                     try:
-                        result[y][x] = 999999  # -MAX
+                        result[y][x] = 99999999  # -MAX
                     except IndexError:  # ignore out of bounds
                         pass
 
@@ -64,6 +73,7 @@ def finding_signs(src):  # capturing the treasure box's sign
 
 
 def finding_gems(src):
+    src = cv.cvtColor(src, cv.COLOR_RGB2GRAY)
     temp1 = cv.imread("./data/gem/crystal.png")
     temp1 = cv.cvtColor(temp1, cv.COLOR_RGB2GRAY)
     h1, w1 = temp1.shape
@@ -72,8 +82,12 @@ def finding_gems(src):
     temp2 = cv.cvtColor(temp2, cv.COLOR_RGB2GRAY)
     h2, w2 = temp2.shape
     temp2 = (temp2, h2, w2)
+    temp3 = cv.imread("./data/gem/emerald.png")
+    temp3 = cv.cvtColor(temp3, cv.COLOR_RGB2GRAY)
+    h3, w3 = temp3.shape
+    temp3 = (temp3, h3, w3)
 
-    temp_all = [temp1, temp2]
+    temp_all = [temp1, temp2, temp3]
 
     result_bunddle = []
     for temp, h, w in temp_all:
@@ -81,7 +95,7 @@ def finding_gems(src):
         results = []
         while True:
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-            if min_val < 200000:
+            if min_val < 100000:
                 sx, sy = min_loc
                 for x in range(sx, sx + w):
                     for y in range(sy, sy + h):
@@ -100,29 +114,37 @@ def finding_gems(src):
 
 
 def finding_entities(src):
-    temp1 = cv.imread("./data/entity/entity.png")
+    temp1 = cv.imread("./data/entity/bat.png")
     temp1 = cv.cvtColor(temp1, cv.COLOR_RGB2GRAY)
+    threashold = [15000, 500000, 100000]
     h1, w1 = temp1.shape
     temp1 = (temp1, h1, w1)
-    temp2 = cv.imread("./data/entity/entity.png")
+    temp2 = cv.imread("./data/entity/golem.png")
     temp2 = cv.cvtColor(temp2, cv.COLOR_RGB2GRAY)
     h2, w2 = temp2.shape
     temp2 = (temp2, h2, w2)
-
-    temp_all = [temp1, temp2]
+    temp3 = cv.imread("./data/entity/golem2.png")
+    temp3 = cv.cvtColor(temp3, cv.COLOR_RGB2GRAY)
+    h2, w2 = temp3.shape
+    temp3 = (temp3, h2, w2)
+    temp_all = [temp1, temp2, temp3]
 
     result_bunddle = []
+    cnt = 0
     for temp, h, w in temp_all:
         result = cv.matchTemplate(src, temp, cv.TM_SQDIFF)
         results = []
+
         while True:
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-            if min_val < 200000:
+            print(min_val)
+            if min_val < threashold[cnt]:
+
                 sx, sy = min_loc
-                for x in range(sx, sx + w):
-                    for y in range(sy, sy + h):
+                for x in range(sx - 50, sx + w + 50):
+                    for y in range(sy - 50, sy + h + 50):
                         try:
-                            result[y][x] = 999999  # -MAX
+                            result[y][x] = 9999999  # -MAX
                         except IndexError:  # ignore out of bounds
                             pass
 
@@ -130,6 +152,7 @@ def finding_entities(src):
                 results.append(res)
             else:
                 break
+        cnt = cnt + 1
         result_bunddle.append(results)
 
     return result_bunddle
@@ -171,13 +194,13 @@ def finding_boss_entities(src):
     return result_bunddle
 
 
-#need to get multiprocessing&monitor capture to build data
+# need to get multiprocessing&monitor capture to build data
 
 
 def main():
     img = cv.imread("test1.png")
 
-    results = finding_boss_entities(img)
+    results = finding_gems(img)
     rand = 200
     for result in results:
         for (x, y, h, w) in result:
