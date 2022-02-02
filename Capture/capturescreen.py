102f521c7864140a8a7ff224c0fe348d61ba7e9d
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-02-03 06:58:40 +0900

    new

--- Capture/capturescreen.py
+++ Capture/capturescreen.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 from multiprocessing import Pool
-
+import pyautogui
 
 def finding_character(src):
     temp = cv.imread("./data/hero.png")
@@ -101,6 +101,7 @@ def finding_gems(src):
                     for y in range(sy - 10, sy + h + 10):
                         try:
                             result[y][x] = 999999  # -MAX
+                            src[y][x] = 9999999
                         except IndexError:  # ignore out of bounds
                             pass
 
@@ -160,9 +161,84 @@ def finding_entities(src):
     temp11 = cv.cvtColor(temp11, cv.COLOR_RGB2GRAY)
     h11, w11 = temp11.shape
     temp11 = (temp11, h11, w11)
-    temp_all = [temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9, temp10, temp11]
+    temp12 = cv.imread("./data/entity/skeleton_l.png")
+    temp12 = cv.cvtColor(temp12, cv.COLOR_RGB2GRAY)
+    h12, w12 = temp12.shape
+    temp12 = (temp12, h12, w12)
+    temp13 = cv.imread("./data/entity/skeleton_r.png")
+    temp13 = cv.cvtColor(temp13, cv.COLOR_RGB2GRAY)
+    h13, w13 = temp13.shape
+    temp13 = (temp13, h13, w13)
+    temp14 = cv.imread("./data/entity/skeleton2_l.png")
+    temp14 = cv.cvtColor(temp14, cv.COLOR_RGB2GRAY)
+    h14, w14 = temp14.shape
+    temp14 = (temp14, h14, w14)
+    temp15 = cv.imread("./data/entity/skeleton2_r.png")
+    temp15 = cv.cvtColor(temp15, cv.COLOR_RGB2GRAY)
+    h15, w15 = temp15.shape
+    temp15 = (temp15, h15, w15)
+    temp16 = cv.imread("./data/entity/skeleton3_l.png")
+    temp16 = cv.cvtColor(temp16, cv.COLOR_RGB2GRAY)
+    h16, w16 = temp16.shape
+    temp16 = (temp16, h16, w16)
+    temp17 = cv.imread("./data/entity/skeleton3_r.png")
+    temp17 = cv.cvtColor(temp17, cv.COLOR_RGB2GRAY)
+    h17, w17 = temp17.shape
+    temp17 = (temp17, h17, w17)
+    temp18 = cv.imread("./data/entity/skeleton4_l.png")
+    temp18 = cv.cvtColor(temp18, cv.COLOR_RGB2GRAY)
+    h18, w18 = temp18.shape
+    temp18 = (temp18, h18, w18)
+    temp19 = cv.imread("./data/entity/skeleton4_r.png")
+    temp19 = cv.cvtColor(temp19, cv.COLOR_RGB2GRAY)
+    h19, w19 = temp19.shape
+    temp19 = (temp19, h19, w19)
+    temp20 = cv.imread("./data/entity/skeleton5_l.png")
+    temp20 = cv.cvtColor(temp20, cv.COLOR_RGB2GRAY)
+    h20, w20 = temp20.shape
+    temp20 = (temp20, h20, w20)
+    temp21 = cv.imread("./data/entity/skeleton5_r.png")
+    temp21 = cv.cvtColor(temp21, cv.COLOR_RGB2GRAY)
+    h21, w21 = temp21.shape
+    temp21 = (temp21, h21, w21)
+    temp22 = cv.imread("./data/entity/skeleton6_l.png")
+    temp22 = cv.cvtColor(temp22, cv.COLOR_RGB2GRAY)
+    h22, w22 = temp22.shape
+    temp22 = (temp22, h22, w22)
+    temp23 = cv.imread("./data/entity/skeleton6_r.png")
+    temp23 = cv.cvtColor(temp23, cv.COLOR_RGB2GRAY)
+    h23, w23 = temp23.shape
+    temp23 = (temp23, h23, w23)
+    temp24 = cv.imread("./data/entity/werewolf_l.png")
+    temp24 = cv.cvtColor(temp24, cv.COLOR_RGB2GRAY)
+    h24, w24 = temp24.shape
+    temp24 = (temp24, h24, w24)
+    temp25 = cv.imread("./data/entity/werewolf_r.png")
+    temp25 = cv.cvtColor(temp25, cv.COLOR_RGB2GRAY)
+    h25, w25 = temp25.shape
+    temp25 = (temp25, h25, w25)
+    temp26 = cv.imread("./data/entity/ghost_l.png")
+    temp26 = cv.cvtColor(temp26, cv.COLOR_RGB2GRAY)
+    h26, w26 = temp26.shape
+    temp26 = (temp26, h26, w26)
+    temp27 = cv.imread("./data/entity/ghost_r.png")
+    temp27 = cv.cvtColor(temp27, cv.COLOR_RGB2GRAY)
+    h27, w27 = temp27.shape
+    temp27 = (temp27, h27, w27)
 
-    threshold = [100000, 1600000, 500000, 500000, 500000, 700000, 500000, 700000, 500000, 1400000, 900000]
+    temp_all = [temp1, temp2, temp3, temp4, temp5,
+                temp6, temp7, temp8, temp9, temp10,
+                temp11,temp12,temp13,temp14,temp15,
+                temp16,temp17,temp18,temp19,temp20,
+                temp21,temp22,temp23,temp24,temp25,
+                temp26,temp27]
+
+    threshold = [1000000, 100000, 900000, 500000, 500000,
+                 700000, 1200000, 600000, 600000, 400000,
+                 900000,6200000,5000000,6000000,5000000,
+                 5500000,5000000,3000000,3500000,2500000,
+                 3000000,2500000,3000000, 600000, 600000,
+                 500000, 300000]
     result_bundle = []
     cnt = 0
     for temp, h, w in temp_all:
@@ -177,7 +253,8 @@ def finding_entities(src):
                 for x in range(sx, sx + w):
                     for y in range(sy, sy + h):
                         try:
-                            result[y][x] = 9999999  # -MAX
+                            result[y][x] = 9999999
+                            src[y][x] = 9999999 # -MAX
                         except IndexError:  # ignore out of bounds
                             pass
 
@@ -230,6 +307,11 @@ def finding_boss_entities(src):
 
 
 # need to get multiprocessing&monitor capture to build data
+def capture_screen():
+    src = pyautogui.screenshot()
+    img_frame = np.array(src)
+    img_frame = cv.cvtColor(img_frame, cv.COLOR_RGB2GRAY)
+    return img_frame
 
 
 def main():
