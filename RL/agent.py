8564c2b049be6b073a07ca0d21dbb2268a0643dd
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-02-03 21:20:23 +0900

    update

--- RL/agent.py
+++ RL/agent.py
@@ -5,29 +5,25 @@ from torch.distributions import Categorical
 import Capture
 import RL.model as module
 
-def run_once():
+
+def run_once(model):
     ts = time.time()
     game_over = False
     while not game_over:
         env = Capture.capture_screen()
-        model = module.A2C()
-        env =torch.from_numpy(env).float()
-        env = torch.reshape(env,(1,1,28,28))
-        prob = model.pi(x=env,softmax_dim=0)
-        prob=Categorical(prob).sample().numpy()
-        if prob ==0:
+        env = torch.from_numpy(env).float()
+        env = torch.reshape(env, (1, 1, 28, 28))
+        prob = model.pi(x=env, softmax_dim=0)
+        prob = Categorical(prob).sample().numpy()
+        if prob == 0:
             pyautogui.press("up")
-        elif prob ==1:
+        elif prob == 1:
             pyautogui.press("down")
-        elif prob ==2:
+        elif prob == 2:
             pyautogui.press("left")
-        elif prob ==3:
+        elif prob == 3:
             pyautogui.press("right")
         game_over = Capture.game_over()
-    tl=time.time()
+    tl = time.time()
 
-    return (tl-ts)/10
-
-if __name__ =="__main__":
-    print(run_once())
-
+    return (tl - ts) / 10
--- play.py
+++ play.py
@@ -1,8 +1,13 @@
 import RL.agent as agent
+import RL.model as module
 import pyautogui
 import Capture
+import torch.optim as optim
 def run():
-    print("시작합니다.")
+    model = module.A2C()
+    optimizer = optim.Adam(model.parameters(),lr= 0.1)
+    reward =agent.run_once(model)
+
 
 
 if __name__ == "__main__":
